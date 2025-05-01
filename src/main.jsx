12f83ee263a9adc5c44391d9b49328e31874fc04
app <app@192.168.1.3>
2025-05-02 00:09:54 +0500

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import "../src/assets/css/style.css";
 import "../src/assets/css/main.css";
 import App from "./App";
 
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
